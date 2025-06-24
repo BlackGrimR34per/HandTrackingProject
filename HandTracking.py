72da66914efa9b4598370dd1d1e35b08af2170ff
Yohen Sheraun <ysheraun20@yahoo.com>
2025-06-24 17:49:53 +0800

    feat: Added hand detection capabilities

---
+++ HandTracking.py
@@ -0,0 +1,24 @@
+import cv2
+import mediapipe as mp
+import time
+
+from numpy.ma.core import resize
+
+cv2.namedWindow("Preview")
+cap = cv2.VideoCapture(2)
+
+mpHands = mp.solutions.hands
+mpDraw = mp.solutions.drawing_utils
+hands = mpHands.Hands()
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                mpDraw.draw_landmarks(img, handLms)
+
+    cv2.imshow("Preview", img)
+    cv2.waitKey(1)
